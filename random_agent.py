b1ff8c9815c815b94c2d024a98b1b57bb1e88f41
Florian CARRA <florianc@sicara.com>
2019-04-19 19:01:47 +0200

    Fix greek letter

--- random_agent.py
+++ random_agent.py
@@ -9,7 +9,7 @@ from helpers.play_helpers import derive_playable_cards
 RANDOM_BET_PROBABILITY = 0.5
 RANDOM_COLOR_WEIGHTS = [1, 1, 1, 1]
 RANDOM_VALUE_NORMAL_MU = 0.
-RANDOM_VALUE_NORMAL_GAMMA = 2.3
+RANDOM_VALUE_NORMAL_SIGMA = 2.3
 
 
 ########
@@ -30,7 +30,7 @@ def bet_or_pass_random_strategy(players_bids):
     if random.random() < RANDOM_BET_PROBABILITY:
         action = 'bet'
         color = random.choices(population=COLORS, weights=RANDOM_COLOR_WEIGHTS, k=1)[0]
-        value = 80 + 10 * round(abs(np.random.normal(loc=RANDOM_VALUE_NORMAL_MU, scale=RANDOM_VALUE_NORMAL_GAMMA)))
+        value = 80 + 10 * round(abs(np.random.normal(loc=RANDOM_VALUE_NORMAL_MU, scale=RANDOM_VALUE_NORMAL_SIGMA)))
         currently_highest_bid_value = derive_currently_highest_bid_value(players_bids)
         if currently_highest_bid_value and (value <= currently_highest_bid_value):
             action = 'pass'
