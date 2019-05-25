a41de5a6e931f19d7fc85b4542b557d10e871673
Florian CARRA <florianc@sicara.com>
2019-05-25 12:16:19 +0200

    Implement yes part of LEVEL 3 for player second in round (0-1-1)

--- expert_agent.py
+++ expert_agent.py
@@ -1,11 +1,14 @@
-from helpers.common_helpers import extract_color
-from helpers.constants import NEXT_PLAYER
+from helpers.common_helpers import extract_color, extract_value
+from helpers.constants import NEXT_PLAYER, TRUMP_POINTS, PLAIN_POINTS, COLORS
 from helpers.play_helpers import derive_playable_cards
 
 
 ########
 # PLAY #
 ########
+
+
+# HELPERS
 
 def derive_player_rank_in_round(round_cards):
     return len([card for card in round_cards.values() if card is not None])
@@ -21,14 +24,39 @@ def is_trump_asked(player, round_cards, trump_color):
     return None
 
 
+def has_trump_in_hand(cards, trump_color):
+    return any([extract_color(card) == trump_color for card in cards])
+
+
+def get_lowest_trump_card(cards, trump_color):
+    def _rank_trump_card(card):
+        return TRUMP_POINTS[extract_value(card)], extract_value(card)
+
+    trump_cards = [card for card in cards if extract_color(card) == trump_color]
+    return min(trump_cards, key=_rank_trump_card)
+
+
+def get_lowest_plain_card(cards):
+    def _rank_plain_card(card):
+        return PLAIN_POINTS[extract_value(card)], extract_value(card), -COLORS.index(extract_color(card))
+
+    return min(cards, key=_rank_plain_card)
+
+
+# LEVEL 1
+
 def play_expert_first_in_round():
     return None
 
 
-def play_expert_second_in_round(trump_asked):
+def play_expert_second_in_round(trump_asked, playable_cards, trump_color):
     # LEVEL 2
     if trump_asked:
-        return None
+        # LEVEL 3
+        if has_trump_in_hand(playable_cards, trump_color):
+            return get_lowest_trump_card(playable_cards, trump_color)
+        else:
+            return get_lowest_plain_card(playable_cards)
     else:
         return None
 
@@ -40,6 +68,8 @@ def play_expert_third_in_round():
 def play_expert_fourth_in_round():
     return None
 
+
+# LEVEL 0
 
 def play_expert_strategy(player, player_cards, cards_playability, round_cards, trump_color):
     playable_cards = derive_playable_cards(player_cards, cards_playability)
@@ -53,7 +83,7 @@ def play_expert_strategy(player, player_cards, cards_playability, round_cards, t
     if player_rank_in_round == 0:
         card = play_expert_first_in_round()
     elif player_rank_in_round == 1:
-        card = play_expert_second_in_round(trump_asked)
+        card = play_expert_second_in_round(trump_asked, playable_cards, trump_color)
     elif player_rank_in_round == 2:
         card = play_expert_third_in_round()
     elif player_rank_in_round == 3:
--- expert_agent_tests.py
+++ expert_agent_tests.py
@@ -1,6 +1,9 @@
 import pytest
 
-from expert_agent import derive_player_rank_in_round, is_trump_asked
+from expert_agent import (
+    derive_player_rank_in_round, is_trump_asked, has_trump_in_hand, get_lowest_trump_card,
+    get_lowest_plain_card,
+)
 
 
 @pytest.mark.parametrize(
@@ -27,3 +30,47 @@ def test_derive_player_rank_in_round(input, expected):
 )
 def test_is_trump_asked(round_cards, trump_color, expected):
     assert is_trump_asked('south', round_cards, trump_color) == expected
+
+
+@pytest.mark.parametrize(
+    'cards, trump_color, expected',
+    [
+        (['10h', 'Kd', '7s'], 'h', True),
+        (['10s', 'Kd', '7s'], 'h', False),
+        ([], 'h', False),
+    ]
+)
+def test_has_trump_in_hand(cards, trump_color, expected):
+    assert has_trump_in_hand(cards, trump_color) == expected
+
+
+@pytest.mark.parametrize(
+    'cards, trump_color, expected',
+    [
+        (['10h', 'Kh', '9h', '7s'], 'h', 'Kh'),
+        (['10h', 'Kh', '9h', '7s', '7h', '8h'], 'h', '7h'),
+    ]
+)
+def test_get_lowest_trump_card(cards, trump_color, expected):
+    assert get_lowest_trump_card(cards, trump_color) == expected
+
+
+def test_get_lowest_trump_card_fails():
+    with pytest.raises(ValueError):
+        get_lowest_trump_card(['10h', 'Kh', '9h', '7s'], 'd')
+
+
+@pytest.mark.parametrize(
+    'cards, expected',
+    [
+        (['10h', 'Kh', '9h', '7s'], '7s'),
+        (['10h', 'Kh', '9h', '7s', '7h', '8d'], '7h'),
+    ]
+)
+def test_get_lowest_plain_card(cards, expected):
+    assert get_lowest_plain_card(cards) == expected
+
+
+def test_get_lowest_plain_card_fails():
+    with pytest.raises(ValueError):
+        get_lowest_plain_card([])
